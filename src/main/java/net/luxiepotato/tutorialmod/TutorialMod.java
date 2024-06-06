a41a69c8b62be029d567c4f64677241d62f929c1
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-06-06 22:13:33 +0200

    Added Ruby Tutorial Group item group

--- src/main/java/net/luxiepotato/tutorialmod/TutorialMod.java
+++ src/main/java/net/luxiepotato/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.luxiepotato.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.luxiepotato.tutorialmod.item.ModItemGroups;
 import net.luxiepotato.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
