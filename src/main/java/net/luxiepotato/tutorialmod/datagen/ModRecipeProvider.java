db45a2c1826ca34f019d2f930e9b0a8c110b2ae4
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-06-12 13:46:50 +0200

    Added Ruby Stairs, Slab, Button, Pressure Plate, Fence, Fence Gate, Wall, Door, Trapdoor Added Ruby Door recipe

--- src/main/java/net/luxiepotato/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/luxiepotato/tutorialmod/datagen/ModRecipeProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
 
@@ -50,5 +51,9 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
                 .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
                 .offerTo(exporter, new Identifier(getRecipeName(ModItems.RAW_RUBY)));
+
+        createDoorRecipe(ModBlocks.RUBY_DOOR, Ingredient.ofItems(ModItems.RUBY))
+                .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.RUBY_DOOR)));
     }
 }
